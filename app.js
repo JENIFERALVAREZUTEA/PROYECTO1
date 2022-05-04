7514767cf328a821bec7bfc6128affa56481efe3
JENIFERALVAREZUTEA <202000202d@utea.edu.pe>
2022-05-04 13:24:54 -0500

    noveno commit:estilo,index,login,log,app,registro modificado

--- app.js
+++ app.js
@@ -1,44 +1,62 @@
-const input = document.querySelector("input");
-const addBtn = document.querySelector(".btn-add");
-const ul = document.querySelector("ul");
-const empty = document.querySelector(".empty");
+window.addEventListener('load', () => {
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const list_el = document.querySelector("#tasks");
 
-addBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
 
-  const text = input.value;
+		const task = input.value;
 
-  if (text !== "") {
-    const li = document.createElement("li");
-    const p = document.createElement("p");
-    p.textContent = text;
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
 
-    li.appendChild(p);
-    li.appendChild(addDeleteBtn());
-    ul.appendChild(li);
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
 
-    input.value = "";
-    empty.style.display = "none";
-  }
+		task_el.appendChild(task_content_el);
+
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input_el);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+		
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
+
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	});
 });
-
-function addDeleteBtn() {
-  const deleteBtn = document.createElement("button");
-
-  deleteBtn.textContent = "X";
-  deleteBtn.className = "btn-delete";
-  editeBtn.
-
-  deleteBtn.addEventListener("click", (e) => {
-    const item = e.target.parentElement;
-    ul.removeChild(item);
-
-    const items = document.querySelectorAll("li");
-
-    if (items.length === 0) {
-      empty.style.display = "block";
-    }
-  });
-
-  return deleteBtn;
-}
